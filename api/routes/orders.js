648690ec35ff3aa99f03e127eea119cc6bd907f4
Ilya Mezheritsky <ilyamez@google.com>
2020-12-31 14:51:07 -0500

    updated var names in orders API

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -25,11 +25,11 @@ router.post("/orderIntent", async function(req, res, next) {
   //console.log("bookID inspect: " + util.inspect(bookId, {showHidden: false, depth: null}))
 
   
-  // hardcode book prices for now
-  const price = new Map([['20201223', '1299'], ['20201224', '11499'], ['20201225', '2499']])
+  // hardcode book prices for now instead of looking them up
+  const bookPrices = new Map([['20201223', '1299'], ['20201224', '11499'], ['20201225', '2499']])
   
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: price.get(body.bookId),
+    amount: bookPrices.get(body.bookId),
     currency: 'usd',
     // Verify your integration in this guide by including this parameter
     metadata: {integration_check: 'accept_a_payment'},
@@ -37,6 +37,43 @@ router.post("/orderIntent", async function(req, res, next) {
     res.json({client_secret: paymentIntent.client_secret});
 
     //res.send('Orders API is working');
+});
+
+
+router.post('/webhook', async (req, res) => {
+  
+  const sig = req.headers['stripe-signature'] as string;
+  const endpointSecret = 'whsec_...';
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(req.body.rawBody, sig, endpointSecret);
+  } catch (err) {
+    res.status(400).end();
+    return;
+  }
+  
+  // Handle Type of webhook
+
+  const intent = event.data.object;
+
+  switch (event.type) {
+    case 'payment_intent.succeeded':
+
+      // Update database
+      // Send email
+      // Notify shipping department
+
+      console.log("Succeeded:", intent.id);
+      break;
+    case 'payment_intent.payment_failed':
+      const message = intent.last_payment_error && intent.last_payment_error.message;
+      console.log('Failed:', intent.id, message);
+      break;
+  }
+
+  res.sendStatus(200);
 });
 
 router.post("/", async function(req, res, next) {
